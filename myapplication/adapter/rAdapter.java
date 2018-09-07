23f65634fad813ae037ad54101ce2f42b7cbc0bf
Zeni patel <38694140+AndroidBasicPrograms@users.noreply.github.com>
2018-09-07 23:13:52 +0530

    Update rAdapter.java

--- myapplication/adapter/rAdapter.java
+++ myapplication/adapter/rAdapter.java
@@ -4,10 +4,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.example.appsb.myapplication.R;
-import com.example.appsb.myapplication.rModel;
+import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
